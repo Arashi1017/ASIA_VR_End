0e37f57143f33663a618d8fbe30974523f618f5e
Arashi1017 <55470682+Arashi1017@users.noreply.github.com>
2021-01-16 02:01:02 +0800

    完成發布

---
+++ VR_End_106051046/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("得分")]
+    public int scoreHit = 5;
+    [Header("玻璃音效")]
+    public AudioClip soundGlass;
+
+    private AudioSource aud;
+    
+    
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="酒瓶")
+        {
+            AddScore();
+        }
+
+        if (other.transform.root.name == "player")
+        {
+            scoreHit = 10;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "player")
+        {
+            scoreHit = 5;
+        }
+    }
+
+
+    private void AddScore()
+    {
+        score += scoreHit;
+        textScore.text = "得分：" + score;
+        aud.PlayOneShot(soundGlass);
+    }
+}
